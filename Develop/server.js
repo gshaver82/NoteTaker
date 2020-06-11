9bbcd72f003fc834b6fbf2ed7b5b32f492eac987
gshaver <geneshaver@gmail.com>
2020-06-11 18:16:09 -0500

    corrected pathing coding for JS to work while hosted

--- Develop/server.js
+++ Develop/server.js
@@ -8,13 +8,14 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
 console.log("log here is working");
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../../notes.html"));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
   console.log("notes html get request is working");
 });
 
