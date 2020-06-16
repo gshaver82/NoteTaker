fa6446b3ac44e3c80028d4a3bfbbe8ccf495b7b0
gshaver <geneshaver@gmail.com>
2020-06-16 14:53:51 -0500

    breakpoint. async awaits working

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var path = require("path");
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const util = require("util");
+const readAsync = util.promisify(fs.readFile);
+const writeAsync = util.promisify(fs.writeFile);
 
 var app = express();
 
@@ -9,36 +14,51 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
-});
+
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
   console.log("notes html get request is working");
 });
 
-app.get("/api/notes", function(req, res) {
-  //should read db.json and return all saved notes as JSON
 
 
-//this is working via postman, but not working on the notes.html page.  the index.js should be 
-//automatically sending a get request to /api/notes, but the below console log doesnt show up
-
-
-  console.log("api notes  get request is working");
-  res.json({ ok: true });
+app.get("/api/notes", async (req, res) => {
+  console.log("api notes get request is working");
+  try {
+    //reads database, parses it and returns it
+    return res.json(JSON.parse(await readAsync(path.join(__dirname, "/db/db.json"), 'utf-8')));
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", async (req, res) => {
   //Should receive a new note to save on the request body, 
   //generate Unique ID and add it to the received object 
   //add that to the db.json file, 
   //and then return the new note to the client.
-  console.log("api notes post request is working");
-  res.json({ ok: true });
+  console.log("inside api notes post request");
+  try {
+    //gets the user inputted note, stringifys and parses it to object, then adds UUID
+    let newNoteObject = JSON.parse(JSON.stringify(req.body));
+    newNoteObject.id = uuidv4();
+    console.log(newNoteObject);
+
+    //reads database file, then parses it and puts it in DBarray
+    let DBArray = JSON.parse(await readAsync(path.join(__dirname, "/db/db.json"), 'utf-8'));
+
+    //appends newNoteObject to the array and then overwrites it to the database 
+    DBArray.push(newNoteObject);
+    await writeAsync(path.join(__dirname, "/db/db.json"), JSON.stringify(DBArray));
+
+    //instructions said to return this?
+    return res.json(newNoteObject);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.delete("/api/notes/:id", function(req, res) {
+app.delete("/api/notes/:id", function (req, res) {
   //Should receive a query parameter containing the id of a note to delete. 
   //This means you'll need to find a way to give each note a unique id when it's saved. 
   //In order to delete a note, you'll need to read all notes from the db.json file, 
@@ -78,3 +98,10 @@ app.get("*", function (req, res) {
 
 //   res.json({ ok: true });
 // });
+
+
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
